c9dc9783e0677a66f058c2a4fdb1dc6a6cd91e45
Jeongsu Yang <mycat83@gmail.com>
2021-08-19 15:48:22 +0900

    feat: Task 기반 배치와 Chunk 기반 배치

---
+++ src/main/java/me/study/springbatch/part3/ChunkProcessingConfiguration.java
@@ -0,0 +1,108 @@
+package me.study.springbatch.part3;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@Slf4j
+public class ChunkProcessingConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    public ChunkProcessingConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    @Bean
+    public Job chunkProcessingJob() {
+        return jobBuilderFactory.get("chunkProcessingJob")
+                .incrementer(new RunIdIncrementer())
+                .start(taskBaseStep())
+                .next(chunkBaseStep())
+                .build();
+    }
+
+    @Bean
+    public Step taskBaseStep() {
+        return stepBuilderFactory.get("taskBaseStep")
+                .tasklet(tasklet())
+                .build();
+    }
+
+    private Tasklet tasklet() {
+        List<String> items = getItems();
+
+        return (contribution, chunkContext) -> {
+            StepExecution stepExecution = contribution.getStepExecution();
+
+            int chunkSize = 10;
+            int fromIndex = stepExecution.getReadCount();
+            int toIndex = fromIndex + chunkSize;
+
+            if (fromIndex >= items.size()) {
+                return RepeatStatus.FINISHED;
+            }
+
+            List<String> subList = items.subList(fromIndex, toIndex);
+
+            log.info("task item size: {}", subList.size());
+
+            stepExecution.setReadCount(toIndex);
+
+            return RepeatStatus.CONTINUABLE;
+        };
+    }
+
+    private List<String> getItems() {
+        List<String> items = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            items.add(i + " Hello");
+        }
+
+        return items;
+    }
+
+    @Bean
+    public Step chunkBaseStep() {
+        return stepBuilderFactory.get("chunkBaseStep")
+                .<String, String>chunk(10)
+                .reader(itemReader())
+                .processor(itemProcessor())
+                .writer(itemWriter())
+                .build();
+    }
+
+    private ItemReader<String> itemReader() {
+        return new ListItemReader<>(getItems());
+    }
+
+    private ItemProcessor<String, String> itemProcessor() {
+        return item -> item + ", SpringBatch";
+    }
+
+    private ItemWriter<? super String> itemWriter() {
+        return items -> {
+            log.info("chunk item size: {}", items.size());
+//            items.forEach(log::info);
+        };
+    }
+}
