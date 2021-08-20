48cf260db5f00d67d04c1b91265b250882b9a5a4
Jeongsu Yang <mycat83@gmail.com>
2021-08-20 11:43:58 +0900

    feat: ItemReader interface 구조

---
+++ src/main/java/me/study/springbatch/part3/ItemReaderConfiguration.java
@@ -0,0 +1,61 @@
+package me.study.springbatch.part3;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Configuration
+@Slf4j
+public class ItemReaderConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    public ItemReaderConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    @Bean
+    public Job itemReaderJob() {
+        return jobBuilderFactory.get("itemReaderJob")
+                .incrementer(new RunIdIncrementer())
+                .start(customItemReaderStep())
+                .build();
+    }
+
+    @Bean
+    public Step customItemReaderStep() {
+        return stepBuilderFactory.get("customItemReaderStep")
+                .<Person, Person>chunk(10)
+                .reader(new CustomItemReader<>(getItems()))
+                .writer(itemWriter())
+                .build();
+    }
+
+    private List<Person> getItems() {
+        List<Person> items = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            items.add(new Person(i + 1, "test name" + i, i + 10, "test address"));
+        }
+
+        return items;
+    }
+
+    private ItemWriter<Person> itemWriter() {
+        return items -> log.info(items.stream()
+                .map(Person::getName)
+                .collect(Collectors.joining(", ")));
+    }
+}
