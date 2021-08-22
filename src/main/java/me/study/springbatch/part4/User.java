8be7a55c89e75c47e49bcbbf425dbc7602ecbf7c
Jeongsu Yang <mycat83@gmail.com>
2021-08-22 21:40:03 +0900

    feat: 회원 데이터 H2 DB에 저장하기

---
+++ src/main/java/me/study/springbatch/part4/User.java
@@ -0,0 +1,53 @@
+package me.study.springbatch.part4;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    @Enumerated(EnumType.STRING)
+    private Level level = Level.NORMAL;
+
+    private int totalAmount;
+
+    private LocalDate updatedDate;
+
+    @Builder
+    private User(String username, int totalAmount) {
+        this.username = username;
+        this.totalAmount = totalAmount;
+    }
+
+    public enum Level {
+        VIP(500_000, null),
+        GOLD(500_000, VIP),
+        SILVER(300_000, GOLD),
+        NORMAL(200_000, SILVER);
+
+        private final int nextAmount;
+        private final Level nextLevel;
+
+        Level(int nextAmount, Level nextLevel) {
+            this.nextAmount = nextAmount;
+            this.nextLevel = nextLevel;
+        }
+    }
+}
