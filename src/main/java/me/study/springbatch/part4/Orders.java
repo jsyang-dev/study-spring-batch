135d50a71973fb51fb1fdf519d636c8ce048ddf2
Jeongsu Yang <mycat83@gmail.com>
2021-09-01 22:47:23 +0900

    feat: User와 Orders Entity의 매핑 관계 개발

---
+++ src/main/java/me/study/springbatch/part4/Orders.java
@@ -0,0 +1,34 @@
+package me.study.springbatch.part4;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Orders {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String itemName;
+
+    private int amount;
+
+    private LocalDate createDate;
+
+    @Builder
+    private Orders(String itemName, int amount, LocalDate createDate) {
+        this.itemName = itemName;
+        this.amount = amount;
+        this.createDate = createDate;
+    }
+}
--- src/main/java/me/study/springbatch/part4/SaveUserTasklet.java
+++ src/main/java/me/study/springbatch/part4/SaveUserTasklet.java
@@ -5,11 +5,14 @@ import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class SaveUserTasklet implements Tasklet {
+
+    public static final int SIZE = 100;
 
     private final UserRepository userRepository;
 
@@ -28,30 +31,46 @@ public class SaveUserTasklet implements Tasklet {
     private List<User> createUsers() {
         List<User> users = new ArrayList<>();
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < SIZE; i++) {
             users.add(User.builder()
-                    .totalAmount(1_000)
+                    .orders(Collections.singletonList(Orders.builder()
+                            .amount(1_000)
+                            .createDate(LocalDate.of(2020, 11, 1))
+                            .itemName("item" + i)
+                            .build()))
                     .username("test username" + i)
                     .build());
         }
 
-        for (int i = 100; i < 200; i++) {
+        for (int i = 0; i < SIZE; i++) {
             users.add(User.builder()
-                    .totalAmount(200_000)
+                    .orders(Collections.singletonList(Orders.builder()
+                            .amount(200_000)
+                            .createDate(LocalDate.of(2020, 11, 2))
+                            .itemName("item" + i)
+                            .build()))
                     .username("test username" + i)
                     .build());
         }
 
-        for (int i = 200; i < 300; i++) {
+        for (int i = 0; i < SIZE; i++) {
             users.add(User.builder()
-                    .totalAmount(300_000)
+                    .orders(Collections.singletonList(Orders.builder()
+                            .amount(300_000)
+                            .createDate(LocalDate.of(2020, 11, 3))
+                            .itemName("item" + i)
+                            .build()))
                     .username("test username" + i)
                     .build());
         }
 
-        for (int i = 300; i < 400; i++) {
+        for (int i = 0; i < SIZE; i++) {
             users.add(User.builder()
-                    .totalAmount(500_000)
+                    .orders(Collections.singletonList(Orders.builder()
+                            .amount(500_000)
+                            .createDate(LocalDate.of(2020, 11, 4))
+                            .itemName("item" + i)
+                            .build()))
                     .username("test username" + i)
                     .build());
         }
